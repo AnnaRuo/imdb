580747bae8461275bd5e1bbaa1de0f399e5e23aa
nojas01 <j.r.nede@gmail.com>
2017-11-07 15:52:13 +0100

    edit seed file with movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Actor.destroy_all
+Movie.destroy_all
 
 
 
-actors =  Actor.create([{ name: 'Brad Pitt'}, { nationality: "American" }, { image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Brad_Pitt_Fury_2014.jpg"}])
+
+movie1 = Movie.create({ name: 'Star Wars' })
+movie2 = Movie.create({ name: "Mr & Ms Smith"})
+movie3 = Movie.create({ name: "Indiana Jones"})
+movie4 = Movie.create({ name: "Leon"})
+movie5 = Movie.create({ name: "Dogma"})
